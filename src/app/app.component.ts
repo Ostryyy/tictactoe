9937383346adc411e417ba2d845d1f84a634db44
OSTROWSKI Arkadiusz <arkadiusz.ostrowski2@soprasteria.com>
2023-01-10 19:31:35 +0100

    fix page display for entering player names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'TicTacToe';
 
-  EnterNamesPageHidden: boolean = true;
+  EnterNamesPageHidden: boolean = false;
   Player1Name: string = '';
   Player2Name: string = '';
 
